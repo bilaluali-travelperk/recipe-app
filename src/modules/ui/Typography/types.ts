6934c4b1d5a7038c39f70d9a4f1d3c4d5fc02315
bilaluali-travelperk <bilal.uali@travelperk.com>
2022-02-04 15:31:11 +0100

    Defined Typography component

---
+++ src/modules/ui/Typography/types.ts
@@ -0,0 +1,7 @@
+import { Variant } from "../Theme";
+
+export interface TypographyProps {
+  variant: Variant;
+  size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'text';
+  children?: React.ReactNode;
+}
